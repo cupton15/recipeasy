83cf3c6c67ad25101e8aa92dab61072fe851fda4
Carl Upton <carlupton15@gmail.com>
2018-06-10 14:30:01 +0100

    working on search bar

---
+++ src/components/RecipeSearch.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import SearchBar from './SearchBar/SearchBar';
+
+class RecipeSearch extends Component {
+    render() {
+        return(
+            <div>
+                <SearchBar placeholder="test"></SearchBar>
+            </div>
+        )
+    }
+}
+
+export default RecipeSearch;
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import './SearchBar.css';
+
+const SearchBar = ({placeholder}) => {
+    return(
+        <div>
+            <input type="text" placeholder="{placeholder}"/> 
+        </div>
+    );
+}
+
+SearchBar.propTypes = {
+    placeholder: PropTypes.string
+}
+
+export default SearchBar;
