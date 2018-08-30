177ec6646bd83474d957b8abdd1a9322c7f854f5
Carl Upton <carlupton15@gmail.com>
2018-08-30 19:19:55 +0100

    working on login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,17 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Form from '../Core/Form/Form';
 import Input from '../Core/Input/Input';
 import Button from '../Core/Button/Button';
 
-const Login = () => {
-  return (
-    <Form title="Login">
-      <Input label="email" name="email" type="email" />
-      <Input label="password" name="password" type="password" />
-      <Button type="submit" text="Log In" />
-    </Form>
-  );
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    const data = this.state;
+    fetch('http://localhost:3300/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => response.json())
+      .then((response) => {
+          console.log(response);
+      })
+      .catch(error => console.error('Error:', error));
+  }
+
+  render() {
+    return (
+      <Form title="Login" onSubmit={this.handleSubmit}>
+        <Input
+          label="email"
+          name="email"
+          type="email"
+          value={this.state.email}
+          onChange={this.handleChange}
+        />
+        <Input
+          label="password"
+          name="password"
+          type="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+        />
+        <Button type="submit" text="Log In" />
+      </Form>
+    );
+  }
 };
 
 export default Login;
