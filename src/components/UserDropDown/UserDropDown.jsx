93e86cc7499a619fc79045820f8c2d082862c22d
carlupton <carlupton15@gmail.com>
2018-09-14 19:35:06 +0100

    adding drop down menu

--- src/components/UserDropDown/UserDropDown.jsx
+++ src/components/UserDropDown/UserDropDown.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { CSSTransition } from 'react-transition-group';
+import classNames from 'classnames';
 
 import './UserDropDown.css';
 
@@ -9,7 +11,12 @@ class UserDropDown extends Component {
 
     this.state = {
       displayName: '',
+      opened: false,
     };
+
+    this.openMenu = this.openMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+    this.logout = this.logout.bind(this);
   }
 
   componentDidMount() {
@@ -34,12 +41,50 @@ class UserDropDown extends Component {
       });
   }
 
+  openMenu(event) {
+    event.preventDefault();
+
+    this.setState({
+      opened: true,
+    }, () => {
+      document.addEventListener('click', this.closeMenu);
+    });
+  }
+
+  closeMenu(event) {
+    if (this.dropdownMenu && !this.dropdownMenu.contains(event.target)) {
+      this.setState({
+        opened: false,
+      }, () => {
+        document.removeEventListener('click', this.closeMenu);
+      });
+    }
+  }
+
+  logout(event) {
+    event.preventDefault();
+
+    this.props.logout();
+  }
+
   render() {
+    const dropDownClass = classNames({
+      'drop-down': true,
+      opened: this.state.opened,
+    });
+
     return (
-      <button className="drop-down-header grow">
-        <span>{this.state.displayName}</span>
-        <FontAwesomeIcon icon="user" size="2x" />
-      </button>
+      <div className={dropDownClass}>
+        <button className="drop-down-header grow" onClick={this.openMenu}>
+          <span>{this.state.displayName}</span>
+          <FontAwesomeIcon icon="user" size="2x" />
+        </button>
+        <CSSTransition in={this.state.opened} classNames="menu" timeout={{ enter: 300, exit: 0 }} unmountOnExit>
+          <div className="menu" ref={(element) => { this.dropdownMenu = element; }}>
+            <button onClick={this.logout}>logout</button>
+          </div>
+        </CSSTransition>
+      </div>
     );
   }
 }
